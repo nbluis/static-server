c0d526a4901c40299601859bd7bce11ff742aebc
yanickrochon <yanick.rochon@gmail.com>
2014-08-14 16:36:01 -0400

    Using streams to serve files. Implemented cache control. Few fixes in file logic. Some output enhencement.

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const DEFAULT_FOLLOW_SYMLINKS = false;
 const DEFAULT_DEBUG = false;
 
 const DEFAULT_STATUS_OK = 200;
+const DEFAULT_STATUS_NOT_MODIFIED = 304;
 const DEFAULT_STATUS_ERR = 500;
 const DEFAULT_STATUS_FORBIDDEN = 403;
 const DEFAULT_STATUS_FILE_NOT_FOUND = 404;
@@ -15,16 +16,23 @@ const VALID_HTTP_METHOD = 'GET';
 const TIME_MS_PRECISION = 3;
 
 
-var http  = require("http");
-var url   = require("url");
-var mime  = require('mime');
-var path  = require("path");
-var fs    = require("fs");
-var fsize = require('file-size');
-var chalk = require('chalk');
-var slice = Array.prototype.slice;
-var program = require('commander')
-  .version('1.0.0')
+var util    = require('util');
+var http    = require("http");
+var url     = require("url");
+var mime    = require('mime');
+var path    = require("path");
+var fs      = require("fs");
+//var buffer  = require('buffer')
+var fsize   = require('file-size');
+var chalk   = require('chalk');
+var slice   = Array.prototype.slice;
+var program = require('commander');
+var pkg     = require(path.join(__dirname, 'package.json'));
+var server;
+
+
+program
+  .version(pkg.name + '@' + pkg.version)
   .usage('[options] <root_path>')
   .option('-p, --port <n>', 'the port to listen to for incoming HTTP connections', DEFAULT_PORT)
   .option('-i, --index <filename>', 'the default index file if not specified', DEFAULT_INDEX)
@@ -32,56 +40,67 @@ var program = require('commander')
   .option('-d, --debug', 'enable to show error messages', DEFAULT_DEBUG)
   .parse(process.argv);
 ;
-var server;
 
 program.rootPath = program.args[0] || process.cwd();
 
+
 initTerminateHandlers();
+createServer();
 
 
 
+/**
+Create the server
+*/
+function createServer() {
+  server = http.createServer(function(req, res) {
+    var uri = req.path = url.parse(req.url).pathname;
+    var filename = path.join(program.rootPath, uri);
+    var timestamp = process.hrtime();
 
-server = http.createServer(function(req, res) {
-  var uri = url.parse(req.url).pathname;
-  var filename = path.join(program.rootPath, uri);
-  var timestamp = process.hrtime();
+    // add a property to get the elapsed time since the request was issued
+    Object.defineProperty(res, 'elapsedTime', {
+      get: function getElapsedTime() {
+        var elapsed = process.hrtime(timestamp);
+        return (elapsed[0] ? elapsed[0] + 's ' : '') + (elapsed[1] / 1000000).toFixed(TIME_MS_PRECISION) + 'ms';
+      }
+    });
 
-  // add a property to get the elapsed time since the request was issued
-  Object.defineProperty(res, 'elapsedTime', {
-    get: function getElapsedTime() {
-      var elapsed = process.hrtime(timestamp);
-      return (elapsed[0] ? elapsed[0] + 's ' : '') + (elapsed[1] / 1000000).toFixed(TIME_MS_PRECISION) + 'ms';
+    res.headers = {
+      'X-Powered-By': pkg.name
+    };
+
+    console.log(chalk.gray('<--'), chalk.blue('[' + req.method + ']'), uri);
+
+    if (req.method !== VALID_HTTP_METHOD) {
+      return sendError(req, res, null, DEFAULT_STATUS_INVALID_METHOD);
+    } else if (!validPath(filename)) {
+      return sendError(req, res, null, DEFAULT_STATUS_FORBIDDEN);
     }
+
+    getFileStats(filename, path.join(filename, program.index), function (err, stat, file, index) {
+      if (err) {
+        sendError(req, res, null, DEFAULT_STATUS_FILE_NOT_FOUND);
+      } else if (stat.isDirectory()) {
+        //
+        // TODO : handle directory listing here
+        //
+        sendError(req, res, null, DEFAULT_STATUS_FORBIDDEN);
+      } else {
+        sendFile(req, res, stat, file);
+      }
+    });
+
   });
 
-  console.log(chalk.gray('<--'), chalk.blue('[' + req.method + ']'), uri);
+  server.listen(program.port);
 
-  if (req.method !== VALID_HTTP_METHOD) {
-    return sendError(res, null, DEFAULT_STATUS_INVALID_METHOD);
-  }
+  console.log(chalk.blue('*'), 'Static server successfully started.');
+  console.log(chalk.blue('*'), 'Listening on port:', chalk.cyan(program.port));
+  console.log(chalk.blue('*'), 'Press', chalk.yellow.bold('Ctrl+C'), 'to shutdown.');
 
-  getFileStats(filename, path.join(filename, program.index), function (err, stats, file) {
-    if (err) {
-      sendError(res, err, DEFAULT_STATUS_FILE_NOT_FOUND);
-    } else if (stats.isDirectory()) {
-      //
-      // TODO : handle directory listing here
-      //
-      sendError(res, null, DEFAULT_STATUS_FORBIDDEN);
-    } else {
-      sendFile(res, stats, file);
-    }
-  });
-
-});
-
-server.listen(program.port);
-
-console.log(chalk.blue('*'), 'Static server successfully started.');
-console.log(chalk.blue('*'), 'Listening on port:', chalk.cyan(program.port));
-console.log(chalk.blue('*'), 'Press', chalk.yellow.bold('Ctrl+C'), 'to shutdown.');
-
-
+  return server;
+}
 
 
 /**
@@ -103,8 +122,10 @@ function initTerminateHandlers() {
 
   // handle INTERRUPT (CTRL+C) and TERM/KILL signals
   process.on('exit', function () {
-    console.log(chalk.blue('*'), 'Shutting down server');
-    server.close();
+    if (server) {
+      console.log(chalk.blue('*'), 'Shutting down server');
+      server.close();
+    }
     console.log();  // extra blank line
   });
   process.on('SIGINT', function () {
@@ -115,6 +136,19 @@ function initTerminateHandlers() {
     console.log(chalk.blue.bold('!'), chalk.yellow.bold('SIGTERM'), 'detected');
     process.exit(0);
   });
+}
+
+
+/**
+Check that path is valid so we don't access invalid resources
+
+@param file {String}        the path to validate
+*/
+function validPath(file) {
+  var resolvedPath = path.resolve(program.rootPath, file);
+
+  // only if we are still in the rootPath of the static site
+  return resolvedPath.indexOf(program.rootPath) === 0;
 }
 
 
@@ -135,7 +169,8 @@ the provided list of files.
 */
 function getFileStats(files, callback) {
   var dirFound;
-  var dirStats;
+  var dirStat;
+  var dirIndex;
 
   if (arguments.length > 2) {
     files = slice.call(arguments, 0, arguments.length - 1);
@@ -147,19 +182,19 @@ function getFileStats(files, callback) {
   function checkNext(err, index) {
     if (files.length) {
       next(files.shift(), index + 1);
-    } else if (err && dirFound) {
+    } else if (dirFound) {
       // if a directory was found at some point, return it and ignore the error
-      callback(null, dirFound, dirStats);
+      callback(null, dirStat, dirFound);
     } else {
       callback(err || new Error('File not found'));
     }
   }
 
   function next(file, index) {
-    fs.lstat(file, function (err, stats) {
+    fs.lstat(file, function (err, stat) {
       if (err) {
-        callback(err);
-      } else if (stats.isSymbolicLink()) {
+        checkNext(err, index);
+      } else if (stat.isSymbolicLink()) {
         if (program.followSymlink) {
           fs.readlink(file, function (err, link) {
             if (err) {
@@ -172,19 +207,65 @@ function getFileStats(files, callback) {
         } else {
           callback(new Error('Symbolic link not allowed'));
         }
-      } else if (stats.isDirectory()) {
+      } else if (stat.isDirectory()) {
         if (!dirFound) {
           dirFound = file;
-          dirStats = stats;
+          dirStat = stat;
+          dirIndex = index;
         }
         checkNext(null, index);
       } else {
-        callback(null, stats, file, index);
+        callback(null, stat, file, index);
       }
     });
   }
 
-  checkNext(0);
+  checkNext(null, 0);
+}
+
+
+/**
+Validate that this file is not client cached
+
+@param req {Object}       the request object
+@param res {Object}       the response object
+@return {boolean}         true if the file is client cached
+*/
+function validateClientCache(req, res, stat) {
+  var mtime         = stat.mtime.getTime();
+  var clientETag  = req.headers['if-none-match'];
+  var clientMTime = Date.parse(req.headers['if-modified-since']);
+
+  if ((clientMTime  || clientETag) &&
+      (!clientETag  || clientETag === res.headers['Etag']) &&
+      (!clientMTime || clientMTime >= mtime)) {
+
+    // NOT MODIFIED responses should not contain entity headers
+    [
+      'Content-Encoding',
+      'Content-Language',
+      'Content-Length',
+      'Content-Location',
+      'Content-MD5',
+      'Content-Range',
+      'Content-Type',
+      'Expires',
+      'Last-Modified'
+    ].forEach(function(entityHeader) {
+        delete res.headers[entityHeader];
+    });
+
+    res.status = DEFAULT_STATUS_NOT_MODIFIED;
+    
+    res.writeHead(res.status, res.headers);
+    res.end();
+
+    console.log(chalk.gray('-->'), chalk.green.dim(res.status, http.STATUS_CODES[res.status]), req.path, '(' + res.elapsedTime + ')');
+
+    return true;
+  } else {
+    return false;
+  }
 }
 
 
@@ -193,24 +274,27 @@ Send error back to the client. If `status` is not specified, a value
 of 500 is used. If `message` is not specified, the default message for
 the given status is returned.
 
-@param res {Object}         the request's response object
+@param req {Object}         the request object
+@param res {Object}         the response object
 @param status {Number}      the status (default 500)
 @param message {String}     the status message (optional)
 */
-function sendError(res, error, status, message) {
-  status = status || DEFAULT_STATUS_ERR;
+function sendError(req, res, error, status, message) {
+  status = status || res.status || DEFAULT_STATUS_ERR
   message = message || http.STATUS_CODES[status];
 
-  console.log(chalk.gray('-->'), chalk.red('ERR'), status, ':', message, '(' + res.elapsedTime + ')');
+  res.headers['Content-Type'] = mime.lookup('text');
 
-  if (error) {
+  res.writeHead(status, res.headers);
+  res.write(message);
+  res.end();
+
+  console.log(chalk.gray('-->'), chalk.red(status, message), req.path, '(' + res.elapsedTime + ')');
+
+  if (error && program.debug) {
     console.error(error.stack || error.message || error);
   }
 
-  res.writeHead(status, {
-    'Content-Type': mime.lookup('text')
-  });
-  res.end(message);
 }
 
 
@@ -219,22 +303,39 @@ Send a file back at the client. If the file is not found, an error 404
 will be returned. If the file cannot be read, for any reason, an error 500
 will be read and the error will be sent to stderr
 
-@param res {Object}        the request's response object
-@param stats {Object}      the actual file stats
+@param req {Object}        the request object
+@param res {Object}        the response object
+@param stat {Object}       the actual file stat
 @param file {String}       the absolute file path
 */
-function sendFile(res, stats, file) {
-  fs.readFile(file, 'binary', function(err, content) {
-    if (err) {
-      return sendError(res, err);
-    }
+function sendFile(req, res, stat, file) {
+  var relFile;
+  var nrmFile;
 
-    res.writeHead(DEFAULT_STATUS_OK, {
-      'Content-type': mime.lookup(file)
-    });
-    res.write(content, 'binary');
-    res.end();
+  res.status = DEFAULT_STATUS_OK;
 
-    console.log(chalk.gray('-->'), 'OK', '"' + path.sep + path.relative(program.rootPath, file) + '"', fsize(stats.size).human(), '(' + res.elapsedTime + ')');
-  });
+  res.headers['Etag']           = JSON.stringify([stat.ino, stat.size, stat.mtime.getTime()].join('-'));
+  res.headers['Date']           = new Date().toUTCString();
+  res.headers['Last-Modified']  = new Date(stat.mtime).toUTCString();
+  res.headers['Content-Type']   = mime.lookup(file);
+  res.headers['Content-Length'] = stat.size;
+
+  if (validateClientCache(req, res, stat, file)) {
+    return;  // abort
+  }
+
+  // TODO : fix this, if the stream encounters an error, headers will be sent twice!
+  res.writeHead(DEFAULT_STATUS_OK, res.headers);
+
+  relFile = path.relative(program.rootPath, file);
+  nrmFile = path.normalize(req.path.substring(1));
+
+  fs.createReadStream(file, {
+    flags: 'r',
+    mode: 0666
+  }).on('close', function () {
+    console.log(chalk.gray('-->'), chalk.green(res.status, http.STATUS_CODES[res.status]), req.path + (nrmFile !== relFile ? (' ' + chalk.dim('(' + relFile + ')')) : ''), fsize(stat.size).human(), '(' + res.elapsedTime + ')');
+  }).on('error', function (err) {
+    sendError(res, err);
+  }).pipe(res);
 };
